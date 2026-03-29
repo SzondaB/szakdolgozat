07d94405c21e05ff9f68e750f0cccde6c7994af8
SzondaB <165283856+SzondaB@users.noreply.github.com>
2026-03-29 23:45:11 +0200

    Update Alice_Bob_Eve.py

--- Alice_Bob_Eve.py
+++ Alice_Bob_Eve.py
@@ -15,12 +15,12 @@ A tréning három elkülönített részből áll:
 3. Alice tanítása olyan reprezentáció előállítására, amely Bob számára jól dekódolható,
    Eve számára viszont nehezebben értelmezhető.
 
-A modell opcionálisan tartalmaz:
-- Eve-loss komponenst,
-- mask regularizációt,
-- ramp/warmup mechanizmust Eve fokozatos bevezetésére.
+A modell tartalmaz 3 darab opcionálisan kikapcsolható komponenst is, melyek az Alice-loss kiszámításában játszanak szerepet:
+- Eve-loss komponens,
+- mask regularizáció,
+- ramp/warmup mechanizmus Eve fokozatos bevezetésére.
 
-A program több seed melletti futtatást támogat, és a végén statisztikai összesítést,
+A program első sorban több seed melletti futtatást támogat, és a végén statisztikai összesítést,
 eloszlásvizsgálatot, valamint grafikus megjelenítést készít az eredményekről.
 """
 
